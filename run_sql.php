0eb70f90be9c98a7f39430eb306707d47240770e
wxv <wxv@users.noreply.github.com>
2015-12-27 00:52:52 -0500

    php load database

--- run_sql.php
+++ run_sql.php
@@ -1,4 +1,27 @@
 <?php
 $item_name = $_POST['item_name'];
-echo $item_name;
+echo $item_name, "\n";
+
+class MyDB extends SQLite3
+{
+	function __construct()
+	{
+		$this->open('data/ah.db');
+	}
+}
+$db = new MyDB();
+if (!$db)
+	echo $db->lastErrorMsg();
+else
+	echo "Database loaded\n";
+	
+$statement = $db->prepare('SELECT * FROM listing WHERE item_name=:item_name');
+$statement->bindValue(':item_name', $item_name);
+$results = $statement->execute();
+
+while($row = $results->fetchArray())
+{
+	var_dump($row);
+}
+		
 ?>
