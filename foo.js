dc59fa8cb2c930915660e60e5e880644f83dc03c
wxv <wxv@users.noreply.github.com>
2015-12-28 17:13:13 -0500

    text center

--- foo.js
+++ foo.js
@@ -4,12 +4,11 @@ function fetchAndGraph() {
 	$.ajax({
 		type: "POST",
 		url: "run_sql.php",
-		async: false,
 		data: {item_name: item_name},
 	})
 	.done(function(response){
-		console.log("response");
-		console.log(response);
+		//console.log("response");
+		//console.log(response);
 		num_array = JSON.parse(response);
 
 		// Convert array of objects to array of arrays
@@ -17,7 +16,7 @@ function fetchAndGraph() {
 			num_array[i] = $.map(num_array[i], function(el) {return el;});
 			num_array[i][0] = new Date(num_array[i][0]*1000);
 		}
-		console.log(num_array);
+		//console.log(num_array);
 
 		g = new Dygraph(document.getElementById("graphdiv"),
 						num_array,
@@ -27,6 +26,7 @@ function fetchAndGraph() {
 							title: "Items Price Over Time",
 							xlabel: "Time and date",
 							ylabel: "Price per unit",
+							maxNumberWidth: 9
 						});		
 	});
 }
